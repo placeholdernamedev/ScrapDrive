b7fdae2594b92544376564b8b4ac95b2e851071f
KonnorBoucher <lonnor1@me.com>
2026-05-05 19:22:27 -0400

    Reworked enemy spawner to work with one or two enemy types, may need debugging once second enemy is added

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject enemyPrefab1;
+    public GameObject enemyPrefab2;
     public Transform player;
     public Transform car;
 
-    public int swarmSize = 5;
+    public int startingSwarm1 = 3;
+    public int startingSwarm2 = 3;
+    public int swarmSize1 = 5;
+    public int swarmSize2 = 5;
     public float spawnRadius = 10f;
     public float spawnInterval = 5f;
 
     private float timer;
+
+
 
     void Update()
     {
@@ -26,14 +32,32 @@ public class EnemySpawner : MonoBehaviour
 
     void SpawnSwarm()
     {
-        for (int i = 0; i < swarmSize; i++)
+        for (int i = 0; i < swarmSize1; i++)
         {
             Vector3 randomOffset = UnityEngine.Random.insideUnitSphere * spawnRadius;
             randomOffset.y = 0;
 
             Vector3 spawnPosition = transform.position + randomOffset;
 
-            GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            GameObject enemy = Instantiate(enemyPrefab1, spawnPosition, Quaternion.identity);
+
+            MeleeEnemyAI enemyAI = enemy.GetComponent<MeleeEnemyAI>();
+
+            if (enemyAI != null)
+            {
+                enemyAI.player = player;
+                enemyAI.car = car;
+            }
+        }
+
+        if (enemyPrefab2 != null)
+        {
+            Vector3 randomOffset = UnityEngine.Random.insideUnitSphere * spawnRadius;
+            randomOffset.y = 0;
+
+            Vector3 spawnPosition = transform.position + randomOffset;
+
+            GameObject enemy = Instantiate(enemyPrefab2, spawnPosition, Quaternion.identity);
 
             MeleeEnemyAI enemyAI = enemy.GetComponent<MeleeEnemyAI>();
 
